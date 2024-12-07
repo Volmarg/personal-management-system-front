88ab3d179d53e415477214872257213ececde055
Volmarg <dwlodarczyk12@gmail.com>
2024-12-07 12:23:00 +0100

    :sparkles: Payments settings - currency multiplier - make it work

--- src/views/Modules/Payments/Components/Settings/TabOther.vue
+++ src/views/Modules/Payments/Components/Settings/TabOther.vue
@@ -4,7 +4,7 @@
       <h2 class="text-lg">{{ $t('payments.settings.tab.other.currencyMultiplier.form.header') }}</h2>
 
       <FormInput type="text"
-                 :model-value="form.currencyMultiplier"
+                 v-model="form.currencyMultiplier"
                  :label="$t('payments.settings.tab.other.currencyMultiplier.form.currencyMultiplier.label')"
       />
 
@@ -13,7 +13,7 @@
                             class="w-full mb-1 md:col-start-1 md:col-end-2 mt-5"
                             button-classes="w-full md:w-auto m-0-force"
                             text-classes="text-center w-full"
-                            @button-click="onNewSubmit"
+                            @button-click="onSubmit"
       />
     </div>
   </div>
@@ -26,6 +26,10 @@ import MediumButtonWithIcon from "@/components/Navigation/Button/MediumButtonWit
 import FormInput            from "@/components/Form/Input.vue";
 
 import {ComponentData} from "@/scripts/Vue/Types/Components/types";
+
+import SymfonyPaymentsRoutes   from "@/router/SymfonyRoutes/Modules/SymfonyPaymentsRoutes";
+import BaseApiResponse         from "@/scripts/Response/BaseApiResponse";
+import BackendModuleCallConfig from "@/scripts/Dto/BackendModuleCallConfig";
 
 export default {
   data(): ComponentData {
@@ -41,11 +45,22 @@ export default {
   },
   methods: {
     /**
-     * @description handle user submitting the new-form
+     * @description sends the multiplier value to the backend
      */
-    onNewSubmit(): void {
-      // todo
+    async onSubmit(): void {
+      let response: BaseApiResponse;
+      let config = new BackendModuleCallConfig(SymfonyPaymentsRoutes.SETTINGS_CURRENCY_MULTIPLIER_BASE_URL, this.id, BaseApiResponse, this.form);
+      config.reload = false;
+
+      response = await this.$moduleCall.update(config);
+      if (response.success) {
+        this.$emit('submit');
+      }
     }
+  },
+  async beforeMount(): Promise<void> {
+    let entry = await this.$moduleCall.get(SymfonyPaymentsRoutes.SETTINGS_CURRENCY_MULTIPLIER_BASE_URL)
+    this.form.currencyMultiplier = entry.value;
   }
 }
 </script>
