b1d0f8626232342c31ed7ee000caf5aab2764b9a
Volmarg <dwlodarczyk12@gmail.com>
2025-03-22 11:20:33 +0100

    :zap: Reduce assets build time, remove obfuscation

--- build-assets.sh
+++ build-assets.sh
@@ -16,11 +16,6 @@ START=$(date +%s) && \
 printInfoBlock "Executing VITE build process" && \
 npx vite build --mode "${ENV_MODE}" && \
  \
-# This has to be made outside vite as otherwise it breaks the js code, consumes tremendous amount of resources & time
-## It can happen that obfuscated code will get slower
-printInfoBlock "Obfuscating code" && \
-npx javascript-obfuscator dist --output=dist && \
- \
 # This allows some browsers to take the gzip version (smaller one)
 printInfoBlock "Compressing files" && \
 gzip --keep dist/assets/*.js && \
