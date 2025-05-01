4330b75deb32e848e089485124fc1ba856e4017d
Volmarg <dwlodarczyk12@gmail.com>
2025-05-01 08:31:12 +0200

    :recycle: Small correction private script for dists

--- private/prepare-prod-dists.bash
+++ private/prepare-prod-dists.bash
@@ -4,12 +4,12 @@ DEV_CONTAINER_NAME='pms-front-dev';
 
 echo -e "Preparing project dist \n";
 
-   docker container stop "$DEV_CONTAINER_NAME" \
+   docker container stop "$DEV_CONTAINER_NAME" || true \
 && docker container start "$PROD_CONTAINER_NAME" \
 && docker exec "$PROD_CONTAINER_NAME" rm -rf ./dist \
 && docker exec "$PROD_CONTAINER_NAME" ./prepare-prod.sh \
 && docker container stop "$PROD_CONTAINER_NAME" \
-&& docker container start "$DEV_CONTAINER_NAME";
+&& docker container start "$DEV_CONTAINER_NAME" || true;
 
 echo -e "Preparing docs dist \n"
 
