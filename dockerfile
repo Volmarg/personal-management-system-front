ae8bfc60ae463fb815ae280800e8e768bbe6d5da
Volmarg <dwlodarczyk12@gmail.com>
2025-08-16 11:35:43 +0200

    :sparkles: Raise node to 18.19.1

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM node:14.21.3-bullseye
+FROM node:18.19.1-bullseye
 
 # # Install some packages for easier work in docker
 RUN apt-get update && apt-get install nano curl -y;
--- private/prepare-prod-dists.bash
+++ private/prepare-prod-dists.bash
@@ -2,17 +2,14 @@
 PROD_CONTAINER_NAME='pms-front-prod';
 DEV_CONTAINER_NAME='pms-front-dev';
 
-echo -e "Preparing project dist \n";
+echo -e "Preparing project and docs dist \n";
 
    docker container stop "$DEV_CONTAINER_NAME" || true \
 && docker container start "$PROD_CONTAINER_NAME" \
 && docker exec "$PROD_CONTAINER_NAME" rm -rf ./dist \
 && docker exec "$PROD_CONTAINER_NAME" ./prepare-prod.sh \
+&& docker exec "$PROD_CONTAINER_NAME" npm run docs:build \
 && docker container stop "$PROD_CONTAINER_NAME" \
 && docker container start "$DEV_CONTAINER_NAME" || true;
-
-echo -e "Preparing docs dist \n"
-
-npm run docs:build;
 
 echo -e "Finished preparing dist data \n"
