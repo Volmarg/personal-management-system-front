b55a59725cd4ab8d40e4f77301d885fd60342ea4
Volmarg <dwlodarczyk12@gmail.com>
2025-09-24 15:36:26 +0200

    :recycle: Explicitly expect discord webhook for notification

--- src/views/System/Settings/Notifications.vue
+++ src/views/System/Settings/Notifications.vue
@@ -4,23 +4,31 @@
 
     <div class="flex justify-center mt-10">
       <div class="md:w-1/2 lg:w-1/3 w-full flex flex-col">
+
         <VueInput type="text"
-                  :label="$t('systemSettings.tab.notifications.config.webhook.input.label')"
-                  :data-type="notification.webhook"
-                  :ref="`${inputPrefix}_${notification.webhook}`"
-                  v-model="inputValues[notification.webhook]"
+                  :label="$t('systemSettings.tab.notifications.config.discordWebhook.input.label')"
+                  :data-type="notification.discordWebhook"
+                  top-wrapper-classes=""
+                  :ref="`${inputPrefix}_${notification.discordWebhook}`"
+                  v-model="inputValues[notification.discordWebhook]"
         />
 
+        <p class="text-red-500 text-center mt-1">
+          {{inputValidationErrors[notification.discordWebhook]}}
+        </p>
+
         <!-- Add multiselect in case when there will be multiple usages for this -->
-        <div class="mb-2 flex flex-row hover:opacity-70 cursor-pointer">
+        <div class="mb-2 flex flex-row hover:opacity-70 cursor-pointer mt-2">
           <Checkbox class="transform scale-75"
-                    v-model="usages[notification.webhook][useCase.reminder]"
+                    v-model="usages[notification.discordWebhook][useCase.reminder]"
           />
-          <label class="checkbox checkbox-all align-self-center ml-1 cursor-pointer"
-          >{{ $t('systemSettings.tab.notifications.config.webhook.checkbox.label') }}</label>
+          <label class="checkbox checkbox-all align-self-center ml-1 cursor-pointer">
+            {{ $t('systemSettings.tab.notifications.config.discordWebhook.checkbox.label') }}
+          </label>
         </div>
 
         <MediumButtonWithIcon :text="$t('systemSettings.tab.notifications.button.submit.label')"
+                              :disabled="Object.keys(inputValidationErrors).length > 0"
                               button-extra-classes="pt-3 pb-3 sm:pt-1 sm:pb-1"
                               class="w-full mb-1 md:col-start-1 md:col-end-2 mt-6 md:pr-0"
                               button-classes="w-full md:w-auto m-0-force"
@@ -38,7 +46,9 @@ import VueInput             from "@/components/Form/Input.vue";
 import Checkbox             from "@/components/Form/Checkbox.vue";
 import MediumButtonWithIcon from "@/components/Navigation/Button/MediumButtonWithIcon.vue";
 
-import DashboardMixin from "@/mixins/DashboardMixin.vue";
+import VuelidateHandler from "@/scripts/Vue/Mixins/VuelidateHandler.vue";
+
+import {helpers} from "@vuelidate/validators";
 
 import {ComponentData} from "@/scripts/Vue/Types/Components/types";
 
@@ -46,12 +56,21 @@ import SymfonySystemRoutes     from "@/router/SymfonyRoutes/Modules/SymfonySyste
 import BaseApiResponse         from "@/scripts/Response/BaseApiResponse";
 import BackendModuleCallConfig from "@/scripts/Dto/BackendModuleCallConfig";
 
+const isDiscordUrl = (url) => new RegExp(/http[s]?:\/\/(www.)?discord\.com\/api\/webhooks\//).test(url);
+
 export default {
   data(): ComponentData {
     return {
+      notification: {
+        discordWebhook: "discordWebhook",
+      },
+      useCase: {
+        reminder: "reminder",
+      },
       loadedConfigs: [],
       inputPrefix: 'configInput',
       inputValues: {},
+      inputValidationErrors: {},
       usages: {},
     }
   },
@@ -61,9 +80,33 @@ export default {
     VueInput
   },
   mixins: [
-    DashboardMixin,
+    VuelidateHandler,
   ],
+  computed: {
+    /**
+     * @description validate inputs
+     */
+    inputValidations(): Record<string, Array> {
+      return {
+        [this.notification.discordWebhook]: [
+            helpers.withMessage(this.$t('systemSettings.tab.notifications.config.discordWebhook.input.validation.notValidUrl'), isDiscordUrl)
+        ],
+      }
+    },
+  },
   methods: {
+    /**
+     * @description will validate the inputs
+     */
+    validate(): void {
+      this.inputValidationErrors = {};
+      for (let type of Object.keys(this.inputValues)) {
+        let errors = this.validateValue(this.inputValues[type], this.inputValidations[type]);
+        if (errors.length > 0) {
+          this.inputValidationErrors[type] = errors[0];
+        }
+      }
+    },
     /**
      * @description reads out the loaded config value of given type or return default data set if config was not found
      */
@@ -83,6 +126,10 @@ export default {
      * @description sends the config values to backend
      */
     onSubmit(): void {
+      if (Object.keys(this.inputValidationErrors).length > 0) {
+        return;
+      }
+
       let inputRefNames = Object.keys(this.$refs).filter((refName: string) => refName.includes(this.inputPrefix));
 
       let configs = [] as Array<Record<string, unknown>>;
@@ -121,11 +168,19 @@ export default {
     }
   },
   beforeMount(): void {
-    this.usages[this.notification.webhook] = {
+    this.usages[this.notification.discordWebhook] = {
       [this.useCase.reminder]: false
     };
 
     this.fetchConfigs();
+  },
+  watch: {
+    inputValues: {
+      deep: true,
+      handler: function () {
+        this.validate();
+      }
+    }
   }
 }
 </script>
