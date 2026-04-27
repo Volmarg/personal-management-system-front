c70b19e602bd8c212400d714d09d6fe6a61c8a04
Volmarg <dwlodarczyk12@gmail.com>
2026-04-27 14:11:30 +0200

    :technologist: Add file path for front playground

--- src/views/Development/Playground.vue
+++ src/views/Development/Playground.vue
@@ -1,6 +1,8 @@
 <template>
   <!-- Keep this page clean, don't let users reach the code that could bypass something here !-->
-  <div></div>
+  <div>
+    See frontend file: <code>src/views/Development/Playground.vue</code>
+  </div>
 </template>
 
 <script lang="ts">
